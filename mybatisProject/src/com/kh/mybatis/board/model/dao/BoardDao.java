521c5256e1e2715241a3f79bb1899c76a316c1bd
JunHong <alswnsghd1234@naver.com>
2022-06-13 23:02:51 +0900

    mybatis study

--- mybatisProject/src/com/kh/mybatis/board/model/dao/BoardDao.java
+++ mybatisProject/src/com/kh/mybatis/board/model/dao/BoardDao.java
@@ -1,11 +1,13 @@
 package com.kh.mybatis.board.model.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 
 import com.kh.mybatis.board.model.vo.Board;
+import com.kh.mybatis.board.model.vo.Reply;
 import com.kh.mybatis.common.model.vo.PageInfo;
 
 public class BoardDao {
@@ -42,5 +44,44 @@ public class BoardDao {
 		
 		return list;
 	}
+
+	public int increaseCount(SqlSession sqlSession, int boardNo) {
+
+		
+		return sqlSession.update("boardMapper.increaseCount",boardNo);
+	}
+	
+	public Board selectBoard(SqlSession sqlSession,int boardNo) {
+		
+		return sqlSession.selectOne("boardMapper.selectDetailBoard",boardNo);
+	}
+
+	public ArrayList<Reply> selectReplyList(SqlSession sqlSession, int boardNo) {
+				
+		return (ArrayList)sqlSession.selectList("boardMapper.selecReplyList",boardNo);
+	}
+
+	public int selectSearchCount(SqlSession sqlSession, HashMap<String, String> map) {
+
+		
+		return sqlSession.selectOne("boardMapper.selectSearchCount",map);
+		
+	}
+
+	public ArrayList<Board> selectsearchBoard(SqlSession sqlSession,HashMap<String, String> map, PageInfo pi) {
+
+		
+		int limit = pi.getBoardLimit();
+		int offset = (pi.getCurrentPage() - 1) * limit;
+		
+		RowBounds rowBounds = new RowBounds(offset,limit);
+		
+		ArrayList<Board> list = (ArrayList)sqlSession.selectList("boardMapper.selectBoardList",map,rowBounds);
+		
+		return list;
+	}
+
+
+
 
 }
--- mybatisProject/src/com/kh/mybatis/board/model/service/BoardServiceImpl.java
+++ mybatisProject/src/com/kh/mybatis/board/model/service/BoardServiceImpl.java
@@ -3,11 +3,13 @@ package com.kh.mybatis.board.model.service;
 import static com.kh.mybatis.common.template.Template.getSqlSession;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.ibatis.session.SqlSession;
 
 import com.kh.mybatis.board.model.dao.BoardDao;
 import com.kh.mybatis.board.model.vo.Board;
+import com.kh.mybatis.board.model.vo.Reply;
 import com.kh.mybatis.common.model.vo.PageInfo;
 
 public class BoardServiceImpl implements BoardService{
@@ -39,14 +41,69 @@ public class BoardServiceImpl implements BoardService{
 
 	@Override
 	public int increaseCount(int boardNo) {
+
+		SqlSession sqlSession = getSqlSession();
+		
+		int result = boardDao.increaseCount(sqlSession,boardNo);
+		
+		if(result>0) {
+			sqlSession.commit();
+		}else {
+			sqlSession.rollback();
+		}
+		
+		sqlSession.close();
+		
+		return result;
+	}
+
+	@Override
+	public Board selectBoard(int boardNo) {
+
+		SqlSession sqlSession = getSqlSession();
+		
+		Board b = boardDao.selectBoard(sqlSession,boardNo);
+		
+		sqlSession.close();
+		
+		return b;
+	}
+
+	@Override
+	public ArrayList<Reply> selectReply(int boardNo) {
+
+		SqlSession sqlSession = getSqlSession();
+		
+		ArrayList<Reply> list = boardDao.selectReplyList(sqlSession,boardNo);
+		
+		sqlSession.close();
+						
+		return list;
+	}
+
+	@Override
+	public ArrayList<Board> selectBoard(PageInfo pi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int selectSearchCount(HashMap<String, String> map) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public Board selectBoard(int boardNo) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Board> selectsearchBoard(HashMap<String, String> map,PageInfo pi) {
+
+		SqlSession sqlSession = getSqlSession();
+		
+		ArrayList<Board> list = boardDao.selectsearchBoard(sqlSession,map,pi);
+		
+		sqlSession.close();
+		
+		return list;
 	}
+	
 
 }
